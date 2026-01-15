2daca2f225d77a5ed8c1d21239f029c884e754c1
EnityBlackHawk <luanf2003@gmail.com>
2026-01-15 12:17:34 +0100

    chore: add enable_minimal_separate_debug_simbols macro

--- RevenantCollectorCppMacros.cmake
+++ RevenantCollectorCppMacros.cmake
@@ -1,12 +1,22 @@
-macro(enable_minimal_debug_simbols target_name tp_debug_symbols)
+macro(enable_minimal_debug_simbols target_name)
     target_compile_options(example-rev PRIVATE -g1)
-    if (tp_debug_symbols STREQUAL "SEPARATE")
 
-        add_custom_command(
-                TARGET ${target_name} POST_BUILD
-                COMMAND ${CMAKE_OBJCOPY} ARGS --only-keep-debug $<TARGET_FILE:${target_name}> $<TARGET_FILE:${target_name}>.debug
-                COMMAND ${CMAKE_OBJCOPY} ARGS --strip-debug $<TARGET_FILE:${target_name}>
-                COMMAND ${CMAKE_OBJCOPY} ARGS --add-gnu-debuglink=$<TARGET_FILE:${target_name}>.debug $<TARGET_FILE:${target_name}>
-        )
-    endif()
+    add_custom_command(
+            TARGET ${target_name} POST_BUILD
+            COMMAND ${CMAKE_OBJCOPY} ARGS --only-keep-debug $<TARGET_FILE:${target_name}> $<TARGET_FILE:${target_name}>.debug
+            COMMAND ${CMAKE_OBJCOPY} ARGS --strip-debug $<TARGET_FILE:${target_name}>
+            COMMAND ${CMAKE_OBJCOPY} ARGS --add-gnu-debuglink=$<TARGET_FILE:${target_name}>.debug $<TARGET_FILE:${target_name}>
+    )
+endmacro()
+
+
+macro(enable_minimal_separate_debug_simbols target_name)
+    target_compile_options(example-rev PRIVATE -g1)
+
+    add_custom_command(
+            TARGET ${target_name} POST_BUILD
+            COMMAND ${CMAKE_OBJCOPY} ARGS --only-keep-debug $<TARGET_FILE:${target_name}> $<TARGET_FILE:${target_name}>.debug
+            COMMAND ${CMAKE_OBJCOPY} ARGS --strip-debug $<TARGET_FILE:${target_name}>
+            COMMAND ${CMAKE_OBJCOPY} ARGS --add-gnu-debuglink=$<TARGET_FILE:${target_name}>.debug $<TARGET_FILE:${target_name}>
+    )
 endmacro()
